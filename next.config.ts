5ca2608f6d55463c07632f39cc423b74b0d1cae2
Abubakar <160392189+Abubakar-shafaqat@users.noreply.github.com>
2025-01-28 12:48:53 -0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['car-rental-website-five.vercel.app' ,'www.pngplay.com' , 'encrypted-tbn0.gstatic.com' , 'ih1.redbubble.net' , 'i.pinimg.com'],
+  },
+};
+
+export default nextConfig;
